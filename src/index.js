0eb5c47d1f4479bd2309e4f0a3f412c384c82d69
Jonathan Rod <jonyrm91@gmail.com>
2020-09-07 22:53:18 +0200

    Basic config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+/* eslint-disable no-console */
 // Arrancar la app
+const app = require('./server');
+
+app.listen(app.get('port'), () => {
+  console.log('Listening on port: 3000');
+});
--- src/server.js
+++ src/server.js
@@ -1,1 +1,28 @@
 // Express - Código del servidor
+
+const express = require('express');
+const path = require('path'); // Setting that allows node to find the 'views' folder in any enviroment.
+
+// Initializations
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, '/views'));
+
+// Middlewares
+// Conversion of any data type that comes from a formulary through methods to JSON.
+app.use(express.urlencoded({ extended: false }));
+
+// Gobal Variables
+
+// Routes
+// URLs
+app.get('/', (req, res) => {
+  res.send('Welcome To Nodeapi');
+});
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
