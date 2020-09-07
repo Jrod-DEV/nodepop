7789a9ec1884e5ba5faed2189607c00b834ceb82
Jonathan Rod <jonyrm91@gmail.com>
2020-09-08 01:14:45 +0200

    Basic conf + .env

--- src/database.js
+++ src/database.js
@@ -1,1 +1,16 @@
+/* eslint-disable no-console */
 // Mongoose
+const mongoose = require('mongoose');
+
+/* const NODDEPOP_API_MONGODB_HOST = process.env.MONGODB_HOST;
+const NODDEPOP_API_MONGODB_DATABASE = process.env.MONGODB_DATABASE; */
+const { NODEPOP_API_MONGODB_HOST, NODEPOP_API_MONGODB_DATABASE } = process.env;
+const MONGODB_URI = `mongodb://${NODEPOP_API_MONGODB_HOST}/${NODEPOP_API_MONGODB_DATABASE}`; // Cadena de conexión a DB
+
+mongoose.connect(MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+})
+
+  .then(db => console.log('Database is connected'))
+  .catch(err => console.log(err));
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 /* eslint-disable no-console */
 // Arrancar la app
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config(); // Carga variables de entorno
+
 const app = require('./server');
+require('./database');
 
 app.listen(app.get('port'), () => {
-  console.log('Listening on port: 3000');
+  console.log('Listening on port:', app.get('port'));
 });
